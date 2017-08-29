be388c42e73539084c77fe05c45ad82fb2c09cd2
jiangbai333@163.com <jiangbai333@163.com>
2017-08-29 14:29:54 +0800

    add some function

--- src/main/java/com/filehandle/FileHandle.java
+++ src/main/java/com/filehandle/FileHandle.java
@@ -1,18 +1,49 @@
 package com.filehandle;
 
 import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by hsoft on 2017/8/28.
  */
 public class FileHandle {
 
-    public File fp = null;
+    private File fp = // 文件句柄
+            null;
+    private List<File> _fps = // 文件句柄集合
+            new ArrayList<File>();
 
+    /**
+     *     提供了静态获取文件句柄的方法
+     * @param path String 文件路径
+     * @return File 文件操作句柄
+     */
     public static File fromPath(String path) {
         File fp =
                 new File(path);
         return fp;
+    }
+
+    /**
+     *     从一个已知对象构建一个FileHandle实例
+     * FileHandle提供此静态方法，以便于获取
+     * 自身实例
+     * @param obj
+     * @return
+     */
+    public static FileHandle fromObject(Object obj) {
+        FileHandle rt = // 将要返回的FileHandle实例
+                null;
+
+        if ( File.class.isInstance(obj) ) {
+            rt = new FileHandle((File) obj);
+        } else if ( String.class.isInstance(obj) ) {
+            rt = new FileHandle((String) obj);
+        } else if ( List.class.isInstance(obj) ) {
+
+        }
+        return rt;
     }
 
     public FileHandle() {}
@@ -22,6 +53,20 @@ public class FileHandle {
                 new File(path);
     }
 
+    public FileHandle(File file) {
+        fp =
+                file;
+    }
+
+    public FileHandle(List<File> files) {
+
+    }
+
+    /**
+     *     返回文件大小
+     * @param unit String 返回值单位标记
+     * @return String 文件大小
+     */
     public String size(String unit) {
         long size =
                 fp.length();
@@ -39,7 +84,19 @@ public class FileHandle {
         return rt;
     }
 
-    public String sayHello() {
-        return "hello world!";
+    /**
+     *     返回文件大小
+     * @return
+     */
+    public Long size() {
+        return fp.length();
+    }
+
+    /**
+     *     返回File实例
+     * @return
+     */
+    public File getFileHandle() {
+        return this.fp;
     }
 }
